c168db4869ab3684fe453ffefdf856d43153c5a4
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2025-01-10 01:50:33 +0200

    delete redundant exception

--- src/main/java/mate/academy/rickandmorty/exception/DataMappingExceptoin.java
+++
@@ -1,7 +0,0 @@
-package mate.academy.rickandmorty.exception;
-
-public class DataMappingExceptoin extends RuntimeException {
-    public DataMappingExceptoin(String message, Exception ex) {
-        super(message, ex);
-    }
-}
