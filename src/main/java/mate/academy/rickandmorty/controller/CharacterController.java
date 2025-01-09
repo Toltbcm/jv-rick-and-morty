1a179187d9953db3b6ad01057661a2bfe7ebf640
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2025-01-10 01:34:52 +0200

    add some documentation for swagger

--- src/main/java/mate/academy/rickandmorty/controller/CharacterController.java
+++ src/main/java/mate/academy/rickandmorty/controller/CharacterController.java
@@ -1,5 +1,7 @@
 package mate.academy.rickandmorty.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import mate.academy.rickandmorty.dto.CharacterResponseDto;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Character management", description = "Endpoint for managing characters")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/characters")
@@ -17,14 +20,15 @@ public class CharacterController {
 
     private final CharacterService characterService;
 
+    @Operation(summary = "Get character by ID", description = "Get character by ID")
     @GetMapping("/{id}")
     public CharacterResponseDto getById(@PathVariable Long id) {
         return characterService.getById(id);
     }
 
+    @Operation(summary = "Get characters by name part", description = "Get character by name part")
     @GetMapping
     public List<CharacterResponseDto> getById(@RequestParam String namePart) {
         return characterService.getByNamePart(namePart);
     }
-
 }
